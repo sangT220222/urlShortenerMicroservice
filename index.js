f7f82b13449847cdf03a05880f928e10b4a6aeb2
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-08 11:54:14 +0100

    Update index.js - post request

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// In-memory storage for URLs - best practice would be use a database
+let url_database = {};
+let url_count = 1;
+
+app.post('/api/shorturl', (req,res) => {
+  const original_url = req.body.url;
+  try{
+    new URL(original_url);
+  }catch(e){
+    return res.json( { error: 'invalid url' });
+  }
+  const short_url = url_count++;
+  url_data[short_url] = original_url;
+  res.json({ original_url : original_url, short_url : short_url});
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
